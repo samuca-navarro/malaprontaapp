5f1490760eb1960e7163b01b8302b7fe788ab2fe
unknown <samuca_navarro@outlook.com>
2020-04-28 00:06:28 -0300

    estrutura básica do projeto

---
+++ src/main/java/main/MalaprontaApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalaprontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalaprontaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/malapronta/MalaprontaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.malapronta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalaprontaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
