f0f799c94f8bb10072db28321444295ebf43e90b
reidomath <mat.s.ferreira@gmail.com>
2020-04-30 22:47:44 -0300

    atualizando Application

--- src/main/java/com/malapronta/MalaProntaApplication.java
+++ src/main/java/com/malapronta/MalaProntaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MalaprontaApplication {
+public class MalaProntaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MalaprontaApplication.class, args);
+		SpringApplication.run(MalaProntaApplication.class, args);
 	}
 	//
 
