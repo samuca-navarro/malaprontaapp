61a39a6c84e8942158d9b584c9476242818579c7
reidomath <mat.s.ferreira@gmail.com>
2020-04-30 22:43:48 -0300

    atualizando nome de pacotes

---
+++ src/main/java/com/malapronta/MalaprontaApplication.java
@@ -0,0 +1,14 @@
+package com.malapronta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalaprontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalaprontaApplication.class, args);
+	}
+	//
+
+}
